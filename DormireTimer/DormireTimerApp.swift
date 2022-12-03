35e62852612ed820a0d443572bf91f98346c7946
kojima rikuto <kojikoji7765@icloud.com>
2022-12-03 15:19:18 +0900

    Initial Commit

---
+++ DormireTimer/DormireTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  DormireTimerApp.swift
+//  DormireTimer
+//
+//  Created by 児島　陸斗 on 2022/12/03.
+//
+
+import SwiftUI
+
+@main
+struct DormireTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
